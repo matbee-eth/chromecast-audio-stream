358b3687c5ca7b3e987df7e8f1a8110074a6a01b
Luigi Poole <luigipoole@outlook.com>
2015-12-02 14:16:40 -0800

    es6 main

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import app from 'app';
 import BrowserWindow from 'browser-window';
 import yargs from 'yargs';
-var lib;
+import {
+    Tray, Menu, MenuItem
+}
+from 'electron';
+import lib from './lib';
 
-const electron = require('electron');
-const Tray = electron.Tray;
-const Menu = electron.Menu;
-const MenuItem = electron.MenuItem;
-var contextMenu = new Menu();
 
 const args = yargs(process.argv.slice(1)).wrap(100).argv;
+const contextMenu = new Menu();
 
 /* Some usefull chrome args */
 app.commandLine.appendSwitch('v', -1);
@@ -19,13 +19,19 @@ app.commandLine.appendSwitch('disable-speech-api');
 app.dock.hide()
 
 app.on('ready', () => {
-    var appIcon = new Tray('icon.png');
+
+    const appIcon = new Tray('icon.png');
+
     appIcon.setContextMenu(contextMenu);
-    lib = require('./lib');
-    lib.on("deviceFound", function (host, devicename) {
-        contextMenu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', click: function () {
-            lib.stream(host);
-        }}));
+
+    lib.on("deviceFound", (host, devicename) => {
+        contextMenu.append(new MenuItem({
+            label: 'MenuItem2',
+            type: 'checkbox',
+            click: () => {
+                lib.stream(host);
+            }
+        }));
         appIcon.setContextMenu(contextMenu);
     });
 });
